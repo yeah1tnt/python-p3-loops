9c8c9638e184be816be93ad8c1f0924dc78f85ca
Tong Tang <pichip22@hotmail.com>
2023-07-05 11:44:51 -0400

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    i = 10
+    while (i > 0):
+        print(i)
+        i -= 1
+    if(i==0):
+        print("Happy New Year!")
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square = []
+    for i in int_list:
+        square.append(i**2)
+    return square
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if (i % 15 == 0):
+            print("FizzBuzz")
+        elif (i % 3 == 0):
+            print("Fizz")
+        elif (i % 5 == 0):
+            print("Buzz")
+        else:
+            print(i)
